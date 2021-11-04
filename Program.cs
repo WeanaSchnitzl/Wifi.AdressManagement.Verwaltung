751da2f848b3a8a05b073f9f9ecb35cb992f412f
Weana Schnitzl <weana.schnitzl@gmail.com>
2021-11-04 20:41:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Wifi.AdressManagement.Verwaltung
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//StreamWriter swData = new StreamWriter("c:\\temp\\test.txt", true);
+			//swData.WriteLine("Hallo Wifi!!!!");
+			//swData.Close();
+
+			StreamReader srData = new StreamReader("c:\\temp\\test.txt");
+			string zeile = srData.ReadLine();
+			srData.Close();
+
+			StreamReader srMails = new StreamReader("K:\\SWE\\mail.txt");
+			while (!srMails.EndOfStream)
+			{
+				zeile = srMails.ReadLine();
+				Console.WriteLine(zeile);
+			}
+			srMails.Close();
+
+			Console.WriteLine(zeile);
+			Console.WriteLine("Press any key...");
+			Console.ReadKey();
+		}
+	}
+
+}
